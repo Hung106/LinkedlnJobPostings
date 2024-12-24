b7c05e55df59ffaf63a485b508ab203910080d63
giaNguyen214 <roy.otaku.214@gmail.com>
2024-12-24 23:43:28 +0700

    add be GN

--- backend/src/routes/order.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const orderController = require('../controller/order.Controller');
-
-router.post('/create', orderController.createOrder);
-
-
-module.exports = router;
