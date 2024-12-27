d1337d6a549d063195ccc21ae7fea74d0beba8ec
Nguyen Kim <117803886+KimPerseus@users.noreply.github.com>
2024-12-27 21:22:20 +0700

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route ,Outlet,Navigate} from 'react-router-dom';
 import Home from './pages/Home';
 import Company from './pages/Company';
+import Job from './pages/Job';
+import Posting from './pages/Posting';
 import DataVisualization from './pages/DataVisualization';
-import PostingTitle from './pages/Title'
-
+//import Posting from './pages/Posting';
 const App = () => {
   return (
     <Router>
       <Routes>
         {/* Public Route */}
         <Route path="/" element={<Home />} />
+        <Route path="/job" element={<Job />} />
+        <Route path="/posting" element={<Posting />} />
         <Route path="/company" element={<Company />} />
-        <Route path="/postingtitle" element={<PostingTitle />} />
         <Route path="/datavisualization" element={<DataVisualization />} />
         {/* Private Routes */}
       </Routes>
