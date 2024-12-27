993621b1162a2df67fbed7eaac96a793dc816bad
Nguyen Kim <117803886+KimPerseus@users.noreply.github.com>
2024-12-27 21:20:58 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route ,Outlet,Navigate} from 'react-router-dom';
+import Home from './pages/Home';
+import Company from './pages/Company';
+import Job from './pages/Job';
+import Posting from './pages/Posting';
+import DataVisualization from './pages/DataVisualization';
+//import Posting from './pages/Posting';
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* Public Route */}
+        <Route path="/" element={<Home />} />
+        <Route path="/job" element={<Job />} />
+        <Route path="/posting" element={<Posting />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="/datavisualization" element={<DataVisualization />} />
+        {/* Private Routes */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
